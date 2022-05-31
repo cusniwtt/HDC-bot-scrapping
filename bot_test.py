aadab1c40283c274c7665a2d685479f98bbe64cd
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-06-01 00:20:27 +0700

    change add1

--- bot_test.py
+++ bot_test.py
@@ -2,12 +2,12 @@ from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
 import time
 
-driver = wd.Edge(r"C:\Users\USER\Documents\edgedriver_win64\msedgedriver.exe")
+driver = wd.Edge(r'edgedriver_win64\msedgedriver.exe')
 
 driver.get('https://hdcservice.moph.go.th/hdc/main/index.php')
 
 add1 = driver.find_element_by_xpath('//*[@id="navbar"]/ul[1]/li[2]/ul/li[1]/ul/li[1]/a')
-add1.click()
+#add1.click()
 
-time.sleep(10)
+time.sleep(5)
 driver.quit()
