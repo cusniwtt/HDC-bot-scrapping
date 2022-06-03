bd0cae6fdacb046a8dba9d7aa1721836f9defb55
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-06-04 01:44:36 +0700

    Finalize BOT

--- downloader_bot.py
+++ downloader_bot.py
@@ -10,13 +10,13 @@ import time
 import os
 
 #Defind function to rename file csv
-def rename_file(name):
+def rename_file(url_path, name):
     year = ['2565', '2564', '2563', '2562', '2561', '2560', '2559', '2558', '2557', '2556']
-    csv_path = [f for f in listdir('dataset') if isfile(join('dataset', f))]
+    csv_path = [f for f in listdir('C:/Users/USER/Downloads') if isfile(join('C:/Users/USER/Downloads', f))]
     new_name = []
     for path in csv_path:  
         if path[:14] == 'exportData_HDC':
-            serve = 'D:/GitHub/HDC-bot-scrapping/dataset/' + path
+            serve = 'C:/Users/USER/Downloads/' + path
             new_name.append(serve)
     first = new_name[9:]
     back = new_name[:9]
@@ -24,7 +24,7 @@ def rename_file(name):
     print(new)
 
     for i in range(10):
-        os.rename(new[i], 'D:/GitHub/HDC-bot-scrapping/dataset/{}_{}.csv'.format(name, year[i]))
+        os.rename(new[i], 'C:/Users/USER/Downloads/dataset/{}_{}_{}.csv'.format(url_path, name, year[i]))
 
 #For create log file
 def write_log(name):
@@ -46,35 +46,31 @@ def bot(url):
     driver = wd.Edge('edgedriver_win64\msedgedriver.exe')
     driver.get(url)
 
-    sp_set = driver.find_element_by_xpath('//*[@id="selSP"]')
+    sp_set = driver.find_element_by_xpath('//*[@id="selSP"]')       #Select Service plan
     sp_set.click()
-    sp_option = driver.find_element_by_xpath('//*[@id="selSP"]/option[3]')
+    sp_option = driver.find_element_by_xpath('//*[@id="selSP"]/option[3]')  #Select each Hospitel
     sp_option.click()
-    pro_set = driver.find_element_by_xpath('//*[@id="selAllProv"]')
+    pro_set = driver.find_element_by_xpath('//*[@id="selAllProv"]') #Select Province
     pro_set.click()
-    pro_option = driver.find_element_by_xpath('//*[@id="selAllProv"]/option[2]')
+    pro_option = driver.find_element_by_xpath('//*[@id="selAllProv"]/option[2]')    #Select each Province
     pro_option.click()
-    time.sleep(3)
 
-    year_set = driver.find_element_by_xpath('//*[@id="selYear"]')
+    year_set = driver.find_element_by_xpath('//*[@id="selYear"]')       #Select Year
     year_set.click()
     
     for i in range(10):
-        opt_xpath = '//*[@id="selYear"]/option[{}]'.format(i+1)
-        year_option = driver.find_element_by_xpath(opt_xpath)
+        opt_xpath = '//*[@id="selYear"]/option[{}]'.format(i+1) #Get year by option
+        year_option = driver.find_element_by_xpath(opt_xpath)   #Select each year
         year_option.click()
-        ok_btn = driver.find_element_by_xpath('//*[@id="btnOkey"]')
+        ok_btn = driver.find_element_by_xpath('//*[@id="btnOkey"]') #click OK to query
         ok_btn.click()
-        time.sleep(10)
+        time.sleep(10)      #Wait for query
 
-        dl_btn = driver.find_element_by_xpath('//*[@id="save-icon"]/button')
+        dl_btn = driver.find_element_by_xpath('//*[@id="save-icon"]/button')    #Click Download button
         dl_btn.click()
 
-        csv_btn = driver.find_element_by_xpath('//*[@id="save-icon"]/ul/li[1]/a')
+        csv_btn = driver.find_element_by_xpath('//*[@id="save-icon"]/ul/li[1]/a')   #Download csv file
         csv_btn.click()
-        time.sleep(1)
-    
-    time.sleep(5)
 
 #Get path of csv file
 csv_path = [f for f in listdir('link_csv/tree_link_csv') if isfile(join('link_csv/tree_link_csv', f))]
@@ -101,7 +97,7 @@ for path in csv_path:       #Loop for each file in csv_path
         bot(url)
 
         #rename file csv that bot download at last
-        rename_file(name)
+        rename_file(path, name)
 
         #Create log of file that bot download is complete
         write_log(name)
