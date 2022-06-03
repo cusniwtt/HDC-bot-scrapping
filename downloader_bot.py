26c79ef6d2702bd2354dc2216d0637b11db2ce08
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-06-04 01:08:06 +0700

    Last Update before run bot

--- downloader_bot.py
+++ downloader_bot.py
@@ -79,27 +79,35 @@ def bot(url):
 #Get path of csv file
 csv_path = [f for f in listdir('link_csv/tree_link_csv') if isfile(join('link_csv/tree_link_csv', f))]
 
-n = 0
-for path in csv_path:
+n = 0       #Set initial of amount file
+for path in csv_path:       #Loop for each file in csv_path
     df = pd.read_csv('link_csv/tree_link_csv/{}'.format(path), sep='|')
 
+    #Create Header of log by path of csv_path
     write_log('############################################################')
     write_log(path)
     write_log('############################################################')
     print('This is download file from {}'.format(path))
 
+    #Loop for each row in df that create from path
     for i in range(df.shape[0]):
-        name = df.iloc[i][0]
-        url = df.iloc[i][1]
+        name = df.iloc[i][0]        #Get name of url
+        url = df.iloc[i][1]         #Get url
 
+        #Check slash and replace it with 'or'
+        name = check_slash(name)
+
+        #run bot to download file
+        bot(url)
+
+        #rename file csv that bot download at last
+        rename_file(name)
+
+        #Create log of file that bot download is complete
         write_log(name)
         print(name)
         print(url)
         n = n + 1
 
-print(n)
-print(len(csv_path))
-
-#name = '1. รายงานสรุป CMI'
-#bot('https://hdcservice.moph.go.th/hdc/reports/report.php?source=pformated/format1.php&cat_id=833e9779725ef96bc06926d8ba4e4c04&id=ce331927a984726e907d00396cacda4d')
-#rename_file(name)
+print('Number of file: {}'.format(n))
+print('Number of CSV sub file: {}'.format(len(csv_path)))
