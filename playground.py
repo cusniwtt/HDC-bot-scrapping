d08f3b882db3264d6f918eae11ed87506c942ee0
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-06-04 23:12:46 +0700

    Update playground

--- playground.py
+++ playground.py
@@ -1,37 +1,15 @@
-from downloader_bot import bot
-from downloader_bot import check_slash
-from downloader_bot import rename_file
-from downloader_bot import write_log
-
+import csv
+from bs4 import BeautifulSoup as bs
+import requests as rq
 import pandas as pd
 import numpy as np
-import os
 from os import listdir
 from os.path import isfile, join
+from selenium import webdriver as wd
+import time
+import os
 
-#Get path of csv file
-csv_path = [f for f in listdir('link_csv/tree_link_csv') if isfile(join('link_csv/tree_link_csv', f))]
-for path in csv_path:
-	if 'access_การบำบัดรักษาและฟื้นฟูผู้ติดยาเสพติดจากระบบ_บสต.csv' == path:
-			df = pd.read_csv('link_csv/tree_link_csv/{}'.format(path), sep='|')
-			#Create sub directory
-			parent_dir = 'C:/Users/USER/Downloads/Dataset/'
-			dir_path = os.path.join(parent_dir, path[:-4])
-			os.mkdir(dir_path)
-
-			#Loop for each row in df that create from path
-			for i in range(df.shape[0]):
-				name = str(df.iloc[i][0])
-				url = str(df.iloc[i][1])
-
-				#Check slash and replace it with 'or'
-				name = check_slash(name)
-
-				#run bot to download file
-				bot(url)
-
-				#rename file csv that bot download at last
-				rename_file(dir_path, name)
-
-				#Create log of file that bot download is complete
-				write_log(name)
+url = 'https://hdcservice.moph.go.th/hdc/main/index.php'
+driver = wd.Edge('edgedriver_win64\msedgedriver.exe')
+driver.get(url)
+time.sleep(5)
