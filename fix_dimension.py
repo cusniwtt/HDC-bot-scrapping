e32e0c24967c788e38cece3406a88328bd0e9a82
Wayu Ragwongsiri <windywayu2000@gmail.com>
2022-07-02 12:03:13 +0700

    IDT-726 remove unnecessary library

--- fix_dimension.py
+++ fix_dimension.py
@@ -1,10 +1,6 @@
-import encodings
 import pandas as pd
-import numpy as np
-import statistics as stat
 import csv
 from glob import glob
-from tqdm import tqdm
 import os
 
 def fix_dimension(filepath):
